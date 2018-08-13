44456bed1d569451d548c13393d5949b1efd69a0
Weinan Li <l.weinan@gmail.com>
2018-08-13 11:36:33 +0800

    .

--- src/main/java/life/jugnu/learnkafka/ch03/FirstProducer.java
+++ src/main/java/life/jugnu/learnkafka/ch03/FirstProducer.java
@@ -11,32 +11,34 @@ class FirstProducer {
     public static void main(String[] args) {
 
         Properties p = new Properties();
-        
+
         // Declare the propeties of cluster and informationa about data key and value
-        p.put("bootstrap.servers","localhost:9092");
-        p.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        p.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        p.put("bootstrap.servers", "localhost:9092");
+        p.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        p.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         p.put("partitioner.class", "life.jugnu.learnkafka.ch03.LogEnabledPartitioner");
 
         // Create producer and send data in format : (topic name , key , value)
-        Producer<String,String> pd = new KafkaProducer<>(p);
-        ProducerRecord<String,String> rec = new ProducerRecord<>("firsttopic" ,"key","value");
+        Producer<String, String> pd = new KafkaProducer<>(p);
+        ProducerRecord<String, String> rec = new ProducerRecord<>("firsttopic", "key", "value");
 
         // Kafka has 3 methods of sending
         // 1) Fire and forget
         pd.send(rec);
 
+        ProducerRecord<String, String> rec2 = new ProducerRecord<>("firsttopic", "foo", "bar");
 
         // 2) Syncronous send , wait for response object
         try {
-            pd.send(rec).get();
+            pd.send(rec2).get();
         } catch (InterruptedException e) {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
 
         // 3 Asynchronous send , give a call back function and track success using call back
-        pd.send(rec,new MyCallback());
+        ProducerRecord<String, String> rec3 = new ProducerRecord<>("firsttopic", "hello", "world");
+        pd.send(rec3, new MyCallback());
         pd.close();
     }
 }
